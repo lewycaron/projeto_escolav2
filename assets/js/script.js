7ecb9e27643ffdd7ce910bfab01e40cf7726c582
lewycaron <lewycaron@gmail.com>
2022-06-17 10:28:57 -0300

    atulalizacao tabela notas

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,9 @@ var json = [
         "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
         "telefone": "41 9999-9999",
         "cidade": "Curitiba",
-        "idade" : "14 anos",
-        "anoLetivo" : "2 Ano",
-        "email": "mcc@gmail.com", 
+        "idade": "14 anos",
+        "anoLetivo": "2 Ano",
+        "email": "mcc@gmail.com",
         "atvExtra": ["Xadrez", "Volei"],
         "materias": [
             {
@@ -18,14 +18,14 @@ var json = [
                 "nomeMat": "matematica",
                 "notaMat": 10,
                 "faltasMat": 0
-            }, 
+            },
             {
                 "idMat": "2",
                 "nomeMat": "Quimica",
                 "notaMat": 9,
                 "faltasMat": 2
             },
-    
+
             {
                 "idMat": "3",
                 "nomeMat": "Fisica",
@@ -33,18 +33,18 @@ var json = [
                 "faltasMat": 5
             }
         ]
-    }, 
-    
+    },
+
     {
-        "id": "2",
+        "id": 2,
         "name": "Acorda Pedrinho",
         "dtNasc": "15 Junho 2000",
         "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
         "telefone": "41 9999-9999",
         "cidade": "Cianorte",
-        "idade" : "22 anos",
-        "anoLetivo" : "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com", 
+        "idade": "22 anos",
+        "anoLetivo": "3 Ano",
+        "email": "wakeuplitlepedro@gmail.com",
         "atvExtra": ["Xadrez", "Futsal"],
         "materias": [
             {
@@ -52,14 +52,14 @@ var json = [
                 "nomeMat": "Matematica",
                 "notaMat": 6,
                 "faltasMat": 6
-            }, 
+            },
             {
                 "idMat": "2",
                 "nomeMat": "Quimica",
                 "notaMat": 7,
                 "faltasMat": 5
             },
-    
+
             {
                 "idMat": "3",
                 "nomeMat": "Fisica",
@@ -69,15 +69,15 @@ var json = [
         ]
     },
     {
-        "id": "3",
+        "id": 3,
         "name": "Acorda Joaquina",
         "dtNasc": "15 Junho 2000",
         "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
         "telefone": "41 9999-9999",
-        "cidade": "Cianorte",
-        "idade" : "22 anos",
-        "anoLetivo" : "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com", 
+        "cidade": "Araucaria",
+        "idade": "22 anos",
+        "anoLetivo": "3 Ano",
+        "email": "wakeuplitlepedro@gmail.com",
         "atvExtra": ["Xadrez", "Futsal"],
         "materias": [
             {
@@ -85,14 +85,14 @@ var json = [
                 "nomeMat": "Matematica",
                 "notaMat": 6,
                 "faltasMat": 6
-            }, 
+            },
             {
                 "idMat": "2",
                 "nomeMat": "Quimica",
                 "notaMat": 7,
                 "faltasMat": 5
             },
-    
+
             {
                 "idMat": "3",
                 "nomeMat": "Fisica",
@@ -101,13 +101,12 @@ var json = [
             }
         ]
     }
-
 ];
 
 // Inserçao de dados dos alunos
 
 var nome = json[1].name;
-var dtNasc = (json[1].dtNasc);
+var dtNasc = json[1].dtNasc;
 var endereco = (json[1].endereco);
 var telefone = (json[1].telefone);
 var cidade = (json[1].cidade);
@@ -126,40 +125,115 @@ document.getElementById('aluno_anoLetivo').innerHTML = anoLetivo;
 document.getElementById('aluno_email').innerHTML = email;
 
 
+let tbody = document.getElementById('tbody');
+for (let i = 0; i < json.length; i++) {
+
+    let tr = tbody.insertRow();
+    let td_nomeAluno = tr.insertCell();
+    let td_nomeDisciplina = tr.insertCell();
+    let td_notaDisciplina = tr.insertCell();
+    let td_faltasDisciplina = tr.insertCell();
+
+    td_nomeAluno.innerText = json[i].name;
+    td_nomeDisciplina.innerText = json[i].materias[i].nomeMat;
+    td_notaDisciplina.innerText = json[i].materias[i].notaMat;
+    td_faltasDisciplina.innerText = json[i].materias[i].faltasMat;
+    
+ 
+}
 
 
 
-// Cadastro de notas e faltas
+//Cadastro de notas e faltas
+class Aluno {
 
 
 
-class Aluno{
 
-    constructor(){
-       this.arrayAluno = {};
+    constructor() {
+        this.id = 1;
+        this.arrayAlunos = {};
     }
+
 
     salvar() {
-        let alunos = this.lerdados();
 
-        console.log(alunos);
+        let dadosalunos = this.lerdados();
+
+
+        if (this.validacampo(alunos)) {
+            this.adicionar(alunos)
+
+        }
+
+        console.log(this.arrayAlunos)
+        this.listaTabela()
     }
-
 
     lerdados() {
 
-        let aluno = {};
-        aluno.nameAluno = document.getElementById('input_nome_aluno').value;
-        aluno.nomeMateria = document.getElementById('input_aluno_materia').value;
-        aluno.notaMateria = document.getElementById('input_aluno_nota').value;
-        aluno.faltasMateria = document.getElementById('input_aluno_falta').value;
-        
-        return aluno;
+        let alunos = {};
+        alunos.id = this.id;
+        alunos.nomeAluno = document.getElementById('input_nome_aluno').value;
+        alunos.nomeDisciplina = document.getElementById('input_aluno_materia').value;
+        alunos.notaDisciplina = document.getElementById('input_aluno_nota').value;
+        alunos.faltasMateria = document.getElementById('input_aluno_falta').value;
+
+        return alunos;
     }
 
 
-    
-    cancelar(){
+
+
+
+    // listaTabela(){
+
+    //     let tbody = document.getElementById('tbody');
+
+    //     for (let i = 0; i < this.arrayAlunos.length; i++) {
+
+    //         let tr = tbody.insertRow();
+    //         let td_nomeAluno = tr.insertCell();
+    //         let td_nomeDisciplina = tr.insertCell();
+    //         let td_notaDisciplina = tr.insertCell();
+    //         let td_faltasDisciplina = tr.insertCell();
+
+    //     }
+
+    // }
+
+    adicionar(alunos) {
+        this.arrayAlunos.push(alunos);
+        this.id++
+    }
+
+
+    validacampo(alunos) {
+
+        let msg = '';
+
+        if (aluno.nomeAluno == '') {
+            msg += 'Informe o Nome do aluno \n';
+        }
+        if (aluno.nomeDisciplina == '') {
+            msg += 'Informe qual a Disciplina \n';
+        }
+        if (aluno.notaDisciplina == '') {
+            msg += 'Informe qual a Nota \n';
+        }
+        if (aluno.faltasMateria == '') {
+            msg += 'Informe a quantidade de faltas \n';
+        }
+
+        if (msg != '') {
+            alert(msg);
+            return false
+        }
+        return true;
+    }
+
+
+    cancelar() {
         alert('teset')
     }
 }
