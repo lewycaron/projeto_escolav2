e761b515ff59cd55317212d804953d57b75c1b46
lewycaron <lewycaron@gmail.com>
2022-06-21 07:14:13 -0300

    alteracoes: responsaveis, alunos, prof, secretaria, json, scripts e imagens

--- assets/js/prof.js
+++ assets/js/prof.js
@@ -1,266 +1,21 @@
+fetch("../../json/aluno.json")
+.then(response => {
+  return response.json();
+})
+.then(json => {
 
 
-var json = [
-    {
-        "id": 1,
-        "categoria":"aluno",
-        "name": "Maria Carolina cardozo",
-        "dtNasc": "17 agosto 2008",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Curitiba",
-        
-        "anoLetivo": "2 Ano",
-        "email": "mcc@gmail.com",
-        "atvExtra": ["Xadrez", "Volei", "Futsal"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "matematica",
-                "notaMat": 10,
-                "faltasMat": 5
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 9,
-                "faltasMat": 2
-            },
-
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 5
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 4,
-                "faltasMat": 10
-            }
-        ]
-    },
-
-    {
-        "id": 2,
-        "categoria":"professor",
-        "name": "Pedrinho",
-        "dtNasc": "15 Junho 2000",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Cianorte",
-        
-        "anoLetivo": "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com",
-        "atvExtra": ["Xadrez", "Futsal"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "Matematica",
-                "notaMat": 6,
-                "faltasMat": 6
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 7,
-                "faltasMat": 5
-            },
-
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 3
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 5,
-                "faltasMat": 5
-            }
-        ]
-    },
-    {
-        "id": 3,
-        "name": "Evelin Valente",
-        "categoria":"aluno",
-        "dtNasc": "15 Junho 2000",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Araucaria",
-        
-        "anoLetivo": "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com",
-        "atvExtra": ["Xadrez", "Judo", "Futsal"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "Matematica",
-                "notaMat": 6,
-                "faltasMat": 6
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 7,
-                "faltasMat": 5
-            },
-
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 3
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 10,
-                "faltasMat": 1
-            }
-        ]
-    },
-    {
-        "id": 4,
-        "name": "Kauan",
-        "categoria":"aluno",
-        "dtNasc": "15 Junho 2000",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Araucaria",
-        
-        "anoLetivo": "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com",
-        "atvExtra": ["Volei", "Futsal"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "Matematica",
-                "notaMat": 6,
-                "faltasMat": 6
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 7,
-                "faltasMat": 5
-            },
-
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 3
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 10,
-                "faltasMat": 1
-            }
-        ]
-    },
-    {
-        "id": 5,
-        "name": "Leandra",
-        "categoria":"Aluno",
-        "dtNasc": "15 Junho 2000",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Araucaria",
-        "idade": "22 anos",
-        "anoLetivo": "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com",
-        "atvExtra": "",
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "Matematica",
-                "notaMat": 6,
-                "faltasMat": 6
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 7,
-                "faltasMat": 5
-            },
-
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 3
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 10,
-                "faltasMat": 1
-            }
-        ]
-    },
-    {
-        "id": 6,
-        "name": "Thais possamai",
-        "categoria":"Aluno",
-        "dtNasc": "15 Junho 2000",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Araucaria",
-        "anoLetivo": "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com",
-        "atvExtra": ["Volei"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "Matematica",
-                "notaMat": 6,
-                "faltasMat": 6
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 7,
-                "faltasMat": 5
-            },
-
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 3
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 10,
-                "faltasMat": 1
-            }
-        ]
-    }
-];
-
-
+var idUser = 2
 
 // Perfil do Usuário
 
-var nome = (json[1].name);
-var dtNasc = json[1].dtNasc;
-var endereco = (json[1].endereco);
-var telefone = (json[1].telefone);
-var cidade = (json[1].cidade);
-var email = (json[1].email);
-
-var anoLetivo = (json[1].anoLetivo);
+var nome = (json[idUser].name);
+var dtNasc = json[idUser].dtNasc;
+var endereco = (json[idUser].endereco);
+var telefone = (json[idUser].telefone);
+var cidade = (json[idUser].cidade);
+var email = (json[idUser].email);
+var anoLetivo = (json[idUser].anoLetivo);
 
 
 // Nome no menu professor
@@ -276,6 +31,9 @@ document.getElementById('prof_nasc').innerHTML = dtNasc;
 document.getElementById('prof_end').innerHTML = endereco;
 document.getElementById('prof_tel').innerHTML = telefone;
 document.getElementById('prof_city').innerHTML = cidade;
-
 document.getElementById('prof_anoLetivo').innerHTML = anoLetivo;
 document.getElementById('prof_email').innerHTML = email;
+
+
+
+})
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,346 +1,103 @@
 // DADOS JSON
-    
-var json = [
-    {
-        "id": 1,
-        "categoria":"aluno",
-        "name": "Maria Carolina cardozo",
-        "dtNasc": "17 agosto 2008",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Curitiba",
-        
-        "anoLetivo": "2 Ano",
-        "email": "mcc@gmail.com",
-        "atvExtra": ["Xadrez", "Volei", "Futsal"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "matematica",
-                "notaMat": 10,
-                "faltasMat": 5
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 9,
-                "faltasMat": 2
-            },
+fetch("../../json/aluno.json")
+.then(response => {
+  return response.json();
+})
 
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 5
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 4,
-                "faltasMat": 10
-            }
-        ]
-    },
+.then(json => {
+ const dadosJson = json
 
-    {
-        "id": 2,
-        "categoria":"professor",
-        "name": "Pedrinho",
-        "dtNasc": "15 Junho 2000",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Cianorte",
-        
-        "anoLetivo": "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com",
-        "atvExtra": ["Xadrez", "Futsal"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "Matematica",
-                "notaMat": 6,
-                "faltasMat": 6
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 7,
-                "faltasMat": 5
-            },
+ var idUser = 1
 
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 3
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 5,
-                "faltasMat": 5
-            }
-        ]
-    },
-    {
-        "id": 3,
-        "name": "Evelin Valente",
-        "categoria":"aluno",
-        "dtNasc": "15 Junho 2000",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Araucaria",
-        
-        "anoLetivo": "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com",
-        "atvExtra": ["Xadrez", "Judo", "Futsal"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "Matematica",
-                "notaMat": 6,
-                "faltasMat": 6
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 7,
-                "faltasMat": 5
-            },
+var nome = (dadosJson[idUser].name);
+var dtNasc = json[idUser].dtNasc;
+var endereco = (json[idUser].endereco);
+var telefone = (json[idUser].telefone);
+var cidade = (json[idUser].cidade);
+var email = (json[idUser].email);
+var idade = (json[idUser].idade);
+var anoLetivo = (json[idUser].anoLetivo);
 
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 3
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 10,
-                "faltasMat": 1
-            }
-        ]
-    },
-    {
-        "id": 4,
-        "name": "Kauan",
-        "categoria":"aluno",
-        "dtNasc": "15 Junho 2000",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Araucaria",
-        
-        "anoLetivo": "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com",
-        "atvExtra": ["Volei", "Futsal"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "Matematica",
-                "notaMat": 6,
-                "faltasMat": 6
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 7,
-                "faltasMat": 5
-            },
-
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 3
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 10,
-                "faltasMat": 1
-            }
-        ]
-    },
-    {
-        "id": 5,
-        "name": "Leandra",
-        "categoria":"Aluno",
-        "dtNasc": "15 Junho 2000",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Araucaria",
-        "idade": "22 anos",
-        "anoLetivo": "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com",
-        "atvExtra": ["Volei", "Futsal"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "Matematica",
-                "notaMat": 6,
-                "faltasMat": 6
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 7,
-                "faltasMat": 5
-            },
-
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 3
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 10,
-                "faltasMat": 1
-            }
-        ]
-    },
-    {
-        "id": 6,
-        "name": "Thais possamai",
-        "categoria":"Aluno",
-        "dtNasc": "15 Junho 2000",
-        "endereco": "R. Prof. Pedro Viriato Parigot de Souza, S/N",
-        "telefone": "41 9999-9999",
-        "cidade": "Araucaria",
-        "anoLetivo": "3 Ano",
-        "email": "wakeuplitlepedro@gmail.com",
-        "atvExtra": ["Volei"],
-        "materias": [
-            {
-                "idMat": 1,
-                "nomeMat": "Matematica",
-                "notaMat": 6,
-                "faltasMat": 6
-            },
-            {
-                "idMat": 2,
-                "nomeMat": "Quimica",
-                "notaMat": 7,
-                "faltasMat": 5
-            },
-
-            {
-                "idMat": 3,
-                "nomeMat": "Fisica",
-                "notaMat": 8,
-                "faltasMat": 3
-            }, 
-            
-            {
-                "idMat": 4,
-                "nomeMat": "Portugues",
-                "notaMat": 10,
-                "faltasMat": 1
-            }
-        ]
-    }
-];
-
-
-
-var nome = (json[1].name);
-var dtNasc = json[1].dtNasc;
-var endereco = (json[1].endereco);
-var telefone = (json[1].telefone);
-var cidade = (json[1].cidade);
-var email = (json[1].email);
-var idade = (json[1].idade);
-var anoLetivo = (json[1].anoLetivo);
 
 
 // TABELAS DE NOTAS E FALTAS
 
-function matematica(){
-    
-    let tbody = document.getElementById('tbody_mat');
+
+    let tbody_mat = document.getElementById('tbody_mat');
     for (let i = 0; i < json.length; i++) {
     
-        let tr = tbody.insertRow();
-        let td_nomeDisciplina = tr.insertCell();
-        let td_nomeAluno = tr.insertCell();
-        let td_notaDisciplina = tr.insertCell();
-        let td_faltasDisciplina = tr.insertCell();
+        let tr_mat = tbody_mat.insertRow();
+        let td_nomeDisciplina_mat = tr_mat.insertCell();
+        let td_nomeAluno_mat = tr_mat.insertCell();
+        let td_notaDisciplina_mat = tr_mat.insertCell();
+        let td_faltasDisciplina_mat = tr_mat.insertCell();
                 
-        td_nomeAluno.innerText = json[i].name; 
-        td_nomeDisciplina.innerText = json[i].materias[0].nomeMat; 
-        td_notaDisciplina.innerText = json[i].materias[0].notaMat;
-        td_faltasDisciplina.innerText = json[i].materias[0].faltasMat;
+        td_nomeAluno_mat.innerText = json[i].name; 
+        td_nomeDisciplina_mat.innerText = json[i].materias[0].nomeMat; 
+        td_notaDisciplina_mat.innerText = json[i].materias[0].notaMat;
+        td_faltasDisciplina_mat.innerText = json[i].materias[0].faltasMat;
     }        
-}
 
 
-function quimica(){
-    let tbody = document.getElementById('tbody_quimica');
+
+
+    let tbody_quim = document.getElementById('tbody_quimica');
     for (let i = 0; i < json.length; i++) {
     
-        let tr = tbody.insertRow();
-        let td_nomeDisciplina = tr.insertCell();
-        let td_nomeAluno = tr.insertCell();
-        let td_notaDisciplina = tr.insertCell();
-        let td_faltasDisciplina = tr.insertCell();
+        let tr_quim = tbody_quim.insertRow();
+        let td_nomeDisciplina_quim = tr_quim.insertCell();
+        let td_nomeAluno_quim = tr_quim.insertCell();
+        let td_notaDisciplina_quim = tr_quim.insertCell();
+        let td_faltasDisciplina_quim = tr_quim.insertCell();
                 
-        td_nomeAluno.innerText = json[i].name; 
-        td_nomeDisciplina.innerText = json[i].materias[1].nomeMat; 
-        td_notaDisciplina.innerText = json[i].materias[1].notaMat;
-        td_faltasDisciplina.innerText = json[i].materias[1].faltasMat;
+        td_nomeAluno_quim.innerText = json[i].name; 
+        td_nomeDisciplina_quim.innerText = json[i].materias[1].nomeMat; 
+        td_notaDisciplina_quim.innerText = json[i].materias[1].notaMat;
+        td_faltasDisciplina_quim.innerText = json[i].materias[1].faltasMat;
     }    
     
-}
 
 
-function fisica(){
-    let tbody = document.getElementById('tbody_fisica');
+
+
+    let tbody_fis = document.getElementById('tbody_fisica');
     for (let i = 0; i < json.length; i++) {
     
-        let tr = tbody.insertRow();
-        let td_nomeDisciplina = tr.insertCell();
-        let td_nomeAluno = tr.insertCell();
-        let td_notaDisciplina = tr.insertCell();
-        let td_faltasDisciplina = tr.insertCell();
+        let tr_fis = tbody_fis.insertRow();
+        let td_nomeDisciplina_fis = tr_fis.insertCell();
+        let td_nomeAluno_fis = tr_fis.insertCell();
+        let td_notaDisciplina_fis = tr_fis.insertCell();
+        let td_faltasDisciplina_fis = tr_fis.insertCell();
         
-        td_nomeAluno.innerText = json[i].name; 
-        td_nomeDisciplina.innerText = json[i].materias[2].nomeMat; 
-        td_notaDisciplina.innerText = json[i].materias[2].notaMat;
-        td_faltasDisciplina.innerText = json[i].materias[2].faltasMat;
+        td_nomeAluno_fis.innerText = json[i].name; 
+        td_nomeDisciplina_fis.innerText = json[i].materias[2].nomeMat; 
+        td_notaDisciplina_fis.innerText = json[i].materias[2].notaMat;
+        td_faltasDisciplina_fis.innerText = json[i].materias[2].faltasMat;
     }    
     
-}
 
-function portugues(){
-    let tbody = document.getElementById('tbody_portugues');
+
+
+    let tbody_port = document.getElementById('tbody_portugues');
     for (let i = 0; i < json.length; i++) {
     
-        let tr = tbody.insertRow();
-        let td_nomeDisciplina = tr.insertCell();
-        let td_nomeAluno = tr.insertCell();
-        let td_notaDisciplina = tr.insertCell();
-        let td_faltasDisciplina = tr.insertCell();
+        let tr_port = tbody_port.insertRow();
+        let td_nomeDisciplina_port = tr_port.insertCell();
+        let td_nomeAluno_port = tr_port.insertCell();
+        let td_notaDisciplina_port = tr_port.insertCell();
+        let td_faltasDisciplina_port = tr_port.insertCell();
         
-        td_nomeAluno.innerText = json[i].name; 
-        td_nomeDisciplina.innerText = json[i].materias[3].nomeMat; 
-        td_notaDisciplina.innerText = json[i].materias[3].notaMat;
-        td_faltasDisciplina.innerText = json[i].materias[3].faltasMat;
+        td_nomeAluno_port.innerText = json[i].name; 
+        td_nomeDisciplina_port.innerText = json[i].materias[3].nomeMat; 
+        td_notaDisciplina_port.innerText = json[i].materias[3].notaMat;
+        td_faltasDisciplina_port.innerText = json[i].materias[3].faltasMat;
     }    
     
-}
+
 
 
 // TABELA DE ATIVIDADES EXTRA 
 
-function atv_Extra(){
     let tbody = document.getElementById('tbody_atvExtra');
     
     for (let i = 0; i < json.length; i++) {
@@ -354,7 +111,10 @@ function atv_Extra(){
         td_atvExtra.innerText = json[i].atvExtra
     }
     
-}
+
+
+})
+
 
 //VALIDADOR DE FORMULÁRIO
 
@@ -372,7 +132,3 @@ window.addEventListener('load', function() {
     }, false);
     });
 }, false);
-
-
-
-
