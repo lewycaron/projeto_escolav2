0731e25e3b041de3c8dc90a2ccff204518645f32
lewycaron <lewycaron@gmail.com>
2022-06-22 02:00:20 -0300

    alteracoes diversas

--- assets/js/agenda.js
+++ assets/js/agenda.js
@@ -9,7 +9,7 @@ function sendForm()
 function insertEvento(data, disciplina, descricao)
 {
     let eventoHtml = 
-        `<div class="mx-5 event alert alert-dark" role="alert" id="event">
+        `<div class="mx-3 event alert alert-dark" role="alert" id="event">
             <h5>${data} - ${disciplina} - ${descricao}</h5>
         </div>`
     document.getElementById("seus-eventos").insertAdjacentHTML("afterend", eventoHtml)
--- assets/js/prof.js
+++ assets/js/prof.js
@@ -1,4 +1,4 @@
-function manipular(json){
+function manipularprof(json){
 
 
 var idUser = 2
@@ -30,14 +30,89 @@ document.getElementById('prof_city').innerHTML = cidade;
 document.getElementById('prof_anoLetivo').innerHTML = anoLetivo;
 document.getElementById('prof_email').innerHTML = email;
 
+// TABELAS DE NOTAS E FALTAS
+
+
+// let tbody_mat = document.getElementById('tbody_mat');
+// for (let i = 0; i < json.length; i++) {
+
+//     let tr_mat = tbody_mat.insertRow();
+//     let td_nomeDisciplina_mat = tr_mat.insertCell();
+//     let td_nomeAluno_mat = tr_mat.insertCell();
+//     let td_notaDisciplina_mat = tr_mat.insertCell();
+//     let td_faltasDisciplina_mat = tr_mat.insertCell();
+            
+//     td_nomeAluno_mat.innerText = json[i].name; 
+//     td_nomeDisciplina_mat.innerText = json[i].materias[0].nomeMat; 
+//     td_notaDisciplina_mat.innerText = json[i].materias[0].notaMat;
+//     td_faltasDisciplina_mat.innerText = json[i].materias[0].faltasMat;
+// }        
+
+
+
+
+// let tbody_quim = document.getElementById('tbody_quimica');
+// for (let i = 0; i < json.length; i++) {
+
+//     let tr_quim = tbody_quim.insertRow();
+//     let td_nomeDisciplina_quim = tr_quim.insertCell();
+//     let td_nomeAluno_quim = tr_quim.insertCell();
+//     let td_notaDisciplina_quim = tr_quim.insertCell();
+//     let td_faltasDisciplina_quim = tr_quim.insertCell();
+            
+//     td_nomeAluno_quim.innerText = json[i].name; 
+//     td_nomeDisciplina_quim.innerText = json[i].materias[1].nomeMat; 
+//     td_notaDisciplina_quim.innerText = json[i].materias[1].notaMat;
+//     td_faltasDisciplina_quim.innerText = json[i].materias[1].faltasMat;
+// }    
+
+
+
+
+
+// let tbody_fis = document.getElementById('tbody_fisica');
+// for (let i = 0; i < json.length; i++) {
+
+//     let tr_fis = tbody_fis.insertRow();
+//     let td_nomeDisciplina_fis = tr_fis.insertCell();
+//     let td_nomeAluno_fis = tr_fis.insertCell();
+//     let td_notaDisciplina_fis = tr_fis.insertCell();
+//     let td_faltasDisciplina_fis = tr_fis.insertCell();
+    
+//     td_nomeAluno_fis.innerText = json[i].name; 
+//     td_nomeDisciplina_fis.innerText = json[i].materias[2].nomeMat; 
+//     td_notaDisciplina_fis.innerText = json[i].materias[2].notaMat;
+//     td_faltasDisciplina_fis.innerText = json[i].materias[2].faltasMat;
+// }    
+
+
+
+
+// let tbody_port = document.getElementById('tbody_portugues');
+// for (let i = 0; i < json.length; i++) {
+
+//     let tr_port = tbody_port.insertRow();
+//     let td_nomeDisciplina_port = tr_port.insertCell();
+//     let td_nomeAluno_port = tr_port.insertCell();
+//     let td_notaDisciplina_port = tr_port.insertCell();
+//     let td_faltasDisciplina_port = tr_port.insertCell();
+    
+//     td_nomeAluno_port.innerText = json[i].name; 
+//     td_nomeDisciplina_port.innerText = json[i].materias[3].nomeMat; 
+//     td_notaDisciplina_port.innerText = json[i].materias[3].notaMat;
+//     td_faltasDisciplina_port.innerText = json[i].materias[3].faltasMat;
+// }    
+
 }
+
+
 fetch('../assets/json/aluno.json')
 .then(response => {
     return response.json();
     })
 .then(json => {
     console.log(json)
-    manipular(json);
+    manipularprof(json);
     })
 .catch(error => {
     console.log(error)
